727af7408a5f333bc79237385e2e98149690734d
Eric Schneider <eric.schneider96@gmail.com>
2021-10-25 23:37:57 +0200

    refactor(html) Move html pages into own dir

--- src/index.ts
+++ src/index.ts
@@ -8,17 +8,16 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 app.get('/', (_,res: Response) => {
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/views/index.html');
 });
 
 app.get('/game/:gameId', (req: Request, res: Response) => {
-  io.once('connection', (socket: Socket) => {
-    socket.join(req.params.gameId);
-    console.log(socket.rooms);
-  });
-  res.sendFile(__dirname + '/game.html');
+    io.once('connection', (socket: Socket) => {
+        socket.join(req.params.gameId);
+    });
+    res.sendFile(__dirname + '/views/game.html');
 });
 
 server.listen(3000, () =>{
-  console.log('listening on *:3000');
-})
+    console.log('listening on *:3000');
+});
